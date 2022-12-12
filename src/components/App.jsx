59d77e76418eccd89a478e791d3fd6ba6b82eb4c
Agustin Auri <agustin_auri@hotmail.com>
2022-12-12 18:22:02 -0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,8 @@ import './App.css';
 import Navbar from './NavBar/NavBar';
 import ItemListContainer from './ItemLIstContainer/ItemListContainer';
 import ItemDetailContainer from './ItemDetailContainer/ItemDetailContainer';
-
-
 const App = () => {
-  
+
   return (
     <>
       <Navbar/>
